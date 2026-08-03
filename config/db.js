5dd3a9caa1150afc66e36c46513547c98b50d970
nikhilkumar72345-create <nikhilkumar72345@gmail.com>
2026-08-03 21:47:05 +0530

    Create config/db.js

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+connection.getConnection((err, conn) => {
+  if (err) {
+    console.error("❌ Database Connection Failed:", err.message);
+  } else {
+    console.log("✅ MySQL Connected");
+    conn.release();
+  }
+});
+
+module.exports = connection;
